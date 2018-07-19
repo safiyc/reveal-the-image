9063de24f73604272642d5fa1b4933428ea0e82b
Safiy Cham <csafiy@yahoo.com>
2018-07-19 14:57:59 -0700

    Add html/css to get better sense of look of page; cleanup done js code

--- js/script.js
+++ js/script.js
@@ -1,57 +1,40 @@
 // gridBox-> height is 400px, width is 400px
 const gridBox = document.querySelector(".grid-box");
+gridBox.style.backgroundImage = "url('./img/soccerball.jpg')";
+
 let columns;
 let row;
 let column;
+let gridSize = 16;
 
-// 4x4
-for (let i = 1; i < 5; i++) {
+// 16x16 grid
+for (let i = 1; i < 17; i++) {
     row = document.createElement("div");
     gridBox.appendChild(row);
     row.classList.add("rows");
-    row.setAttribute("style", `height: ${400 / 4}px`);
+    row.setAttribute("style", `height: ${400 / gridSize}px`);
     row.setAttribute("id", i);
     
-    for (let j = 1; j < 5; j++) {
+    for (let j = 1; j < 17; j++) {
         column = document.createElement("div");
         row.appendChild(column);
         column.classList.add("columns");
-        column.setAttribute("style", `width: ${400 / 4}px`);
+        column.setAttribute("style", `width: ${400 / gridSize}px`);
         column.setAttribute("id", `${i}-${j}`);
-        column.textContent = column.id;
-        // column.setAttribute("style", "background-color: green");
+        column.setAttribute("value", "1");
     }
 }
 
 columns = document.querySelectorAll(".columns");
 
+// click div to make transparent
 columns.forEach(function (column) {
     column.addEventListener("click", function () {
-        if (column.textContent === column.id) {
-            column.style.backgroundColor = "yellow";
-            console.log("color ", column.style.backgroundColor = "yellow");
-            column.textContent = "hi";
-        } else if (column.textContent === "hi") {
-            column.textContent = "hi again";
-            column.style.backgroundColor = "blue";
+        if (column.getAttribute("value") === "1") {
+            column.style.background = "radial-gradient(rgba(0, 255, 0, .35), rgba(0, 255, 0, .45), rgba(0, 255, 0, .85) 65%)";
+            column.setAttribute("value", "2");
+        } else if (column.getAttribute("value") === "2") {
+            column.style.background = "rgba(0, 255, 0, 0)";
         }
     });
 });
-
-// 16x16
-// for (let i = 1; i < 17; i++) {
-//     const row = document.createElement("div");
-//     row.classList.add("rows");
-//     row.setAttribute("style", `height: ${400/16}px`);
-//     row.setAttribute("id", i);
-//     gridBox.appendChild(row);
-
-//     for (let j = 1; j < 17; j++) {
-//         const column = document.createElement("div");
-//         column.classList.add("columns");
-//         column.setAttribute("style", `width: ${400/16}px`);
-//         column.setAttribute("id", `${i}-${j}`);
-//         column.textContent = `${i}-${j}`;
-//         row.appendChild(column);
-//     }
-// }
