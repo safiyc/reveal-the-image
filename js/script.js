edea8b5ac29f8a1c6f717a7897eddc1c0cb4ac9e
Safiy Cham <csafiy@yahoo.com>
2018-07-20 00:03:01 -0700

    Click on block and score goes down. Create function to reset blocks to green and score to 100

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 // gridBox-> height is 400px, width is 400px
 const gridBox = document.querySelector(".grid-box");
 gridBox.style.backgroundImage = "url('./img/soccerball.jpg')";
+let score = document.getElementById("score");
 
 let columns;
 let row;
 let column;
 let gridSize = 16;
+
+score = 100;
+document.getElementById("score").innerHTML = score;
 
 // 16x16 grid
 for (let i = 1; i < 17; i++) {
@@ -27,14 +31,29 @@ for (let i = 1; i < 17; i++) {
 
 columns = document.querySelectorAll(".columns");
 
-// click div to make transparent
+// click div to make transparent, reduce score
 columns.forEach(function (column) {
     column.addEventListener("click", function () {
         if (column.getAttribute("value") === "1") {
-            column.style.background = "radial-gradient(rgba(0, 255, 0, .35), rgba(0, 255, 0, .45), rgba(0, 255, 0, .85) 65%)";
             column.setAttribute("value", "2");
+            column.style.background = "radial-gradient(rgba(0, 255, 0, .35), rgba(0, 255, 0, .45), rgba(0, 255, 0, .85) 65%)";
+            score -= 2;
         } else if (column.getAttribute("value") === "2") {
+            column.setAttribute("value", "3");
             column.style.background = "rgba(0, 255, 0, 0)";
+            score -= 4;
+        } else if (column.getAttribute("value") === "3") {
+            return;
         }
+        document.getElementById("score").innerHTML = score;
+    });
+});
+
+// to reset blocks to green and score to 100
+const reset = document.getElementById("reset");
+reset.addEventListener("click", function () {
+    columns.forEach(function (column) {
+        column.setAttribute("value", "1");
+        column.style.background = "green";
     });
 });
