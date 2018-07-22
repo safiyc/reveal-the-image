b9991fb271b2db52aef0516434aa056bbdd5abb5
Safiy Cham <csafiy@yahoo.com>
2018-07-22 12:23:45 -0700

    Implement score and countdown timer

--- js/script.js
+++ js/script.js
@@ -1,28 +1,40 @@
 // gridBox-> height is 400px, width is 400px
-const gridBox = document.querySelector(".grid-box");
 // need to create function to randomize image & radio btn options
-gridBox.style.backgroundImage = "url('./img/soccerball.jpg')";
-
+const gridBox = document.querySelector(".grid-box");
 const normal = document.querySelector("#normal");
 const hard = document.querySelector("#hard");
-// const reset = document.getElementById("reset");
-// let score = document.getElementById("score");
 
-let columns;
+let gridSize = 16;
+let timer = 5;
+let score = 100;
 let row;
 let column;
-let gridSize = 16;
-let levels;
+let columns;
 
-let score = 100;
-document.getElementById("score").innerHTML = score;
+gridBox.style.backgroundImage = "url('./img/soccerball.jpg')";
+document.getElementById("timer").textContent = timer;
+document.getElementById("score").textContent = score;
 
+// timer
+let timerCountDown = setInterval(function () {
+    timer--;
+    score--;
+    document.getElementById("timer").textContent = timer;
+    document.getElementById("score").textContent = score;
+
+    if (timer <= 0) {
+        columns.forEach(function (column) {
+            column.disabled = true;
+        });
+    }
+}, 1000);
+
+// difficulty options
 normal.addEventListener("click", function () {
     resetIt();
     gridSize = 16;
     createGrid(gridSize);
 });
-
 hard.addEventListener("click", function () {
     resetIt();
     gridSize = 24;
@@ -38,20 +50,19 @@ function createGrid (gridSize) {
         row.setAttribute("id", i);
         
         for (let j = 1; j < (gridSize + 1); j++) {
-            column = document.createElement("div");
+            column = document.createElement("button");
             row.appendChild(column);
-            column.classList.add("columns");
+            column.classList.add("column-btns");
             column.setAttribute("style", `width: ${400 / gridSize}px`);
-            column.setAttribute("id", `${i}-${j}`);
             column.setAttribute("value", "1");
         }
     }
     breakBlock();
 }
 
-// click div to make transparent; score is reduced
+// click block to reduce opacity; score is reduced
 function breakBlock () {
-    columns = document.querySelectorAll(".columns");
+    columns = document.querySelectorAll(".column-btns");
     columns.forEach(function (column) {
         column.addEventListener("click", function () {
             if (column.getAttribute("value") === "1") {
@@ -61,33 +72,24 @@ function breakBlock () {
             } else if (column.getAttribute("value") === "2") {
                 column.setAttribute("value", "3");
                 column.style.background = "rgba(0, 255, 0, 0)";
-                score -= 4;
+                score -= 3;
             } else if (column.getAttribute("value") === "3") {
                 return;
             }
-            document.getElementById("score").innerHTML = score;
         });
     });
 }
 
-// to reset blocks to green and score to 100
+// reset timer, score, and blocks to green
 function resetIt () {
     while (gridBox.hasChildNodes()) {
         gridBox.removeChild(gridBox.firstChild);
     }
 
+    timer = 5;
     score = 100;
-    document.getElementById("score").innerHTML = score;
-
-    // gridSize = 16;
-    // createGrid(gridSize);
+    document.getElementById("timer").textContent = timer;
+    document.getElementById("score").textContent = score;
 }
-
-// reset.addEventListener("click", function () {
-//     resetIt();
-// });
-
-// resetIt();
-
 
 createGrid(gridSize);
