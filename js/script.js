089d59cd7b9e67a2bc68fc67b4ad787f40385fd1
Safiy Cham <csafiy@yahoo.com>
2018-07-26 00:28:22 -0700

    Make timer countdown after 1st block click; timer stops/resets at click of difficulty/reset btns

--- js/script.js
+++ js/script.js
@@ -1,20 +1,43 @@
-// gridBox-> height is 400px, width is 400px
-// need to create function to randomize image & radio btn options
+// gridBox-> 400px by 400px
 const gridBox = document.querySelector(".grid-box");
 const normal = document.querySelector("#normal");
 const hard = document.querySelector("#hard");
 
-let gridSize = 16;
-let timer = 5;
-let score = 100;
-let row;
-let column;
+let g = 16   // blocks in square grid
+let t = 10;   // time in seconds
+let s = 500;   // score
+let gridSize = g;
+let timeAmount = t;
+let score = s;
+let timer;
 let columns;
 
-document.getElementById("timer").textContent = timer;
-document.getElementById("score").textContent = score;
+// create grid; default 16x16 blocks
+function createGrid (gridSize) {
+    let row;
+    let column;
 
+    for (let i = 1; i < (gridSize + 1); i++) {
+        row = document.createElement("div");
+        gridBox.appendChild(row);
+        row.classList.add("rows");
+        row.setAttribute("style", `height: ${400/gridSize}px`);
+        
+        for (let j = 1; j < (gridSize + 1); j++) {
+            column = document.createElement("button");
+            row.appendChild(column);
+            column.classList.add("column-btns");
+            column.setAttribute("style", `width: ${400/gridSize}px`);
+            column.setAttribute("value", "1");
+        }
+    }
 
+    document.getElementById("time-amount").textContent = timeAmount;
+    document.getElementById("score").textContent = score;
+
+    randomBgImg();
+    breakBlock();
+}
 
 // randomize gridBox bg image
 function randomBgImg () {
@@ -35,82 +58,79 @@ function randomBgImg () {
     }
 }
 
-// timer
-let timerCountDown = setInterval(function () {
-    timer--;
-    score--;
-    document.getElementById("timer").textContent = timer;
-    document.getElementById("score").textContent = score;
-
-    if (timer <= 0) {
-        columns.forEach(function (column) {
-            column.disabled = true;
-        });
-    }
-}, 1000);
-
-// difficulty options
-normal.addEventListener("click", function () {
-    resetIt();
-    gridSize = 16;
-    createGrid(gridSize);
-});
-hard.addEventListener("click", function () {
-    resetIt();
-    gridSize = 24;
-    createGrid(gridSize);
-})
-
-function createGrid (gridSize) {
-    for (let i = 1; i < (gridSize + 1); i++) {
-        row = document.createElement("div");
-        gridBox.appendChild(row);
-        row.classList.add("rows");
-        row.setAttribute("style", `height: ${400 / gridSize}px`);
-        row.setAttribute("id", i);
-        
-        for (let j = 1; j < (gridSize + 1); j++) {
-            column = document.createElement("button");
-            row.appendChild(column);
-            column.classList.add("column-btns");
-            column.setAttribute("style", `width: ${400 / gridSize}px`);
-            column.setAttribute("value", "1");
-        }
-    }
-    randomBgImg();
-    breakBlock();
-}
-
 // click block to reduce opacity; score is reduced
 function breakBlock () {
     columns = document.querySelectorAll(".column-btns");
-    columns.forEach(function (column) {
-        column.addEventListener("click", function () {
-            if (column.getAttribute("value") === "1") {
-                column.setAttribute("value", "2");
-                column.style.background = "radial-gradient(rgba(0, 255, 0, .35), rgba(0, 255, 0, .45), rgba(0, 255, 0, .85) 65%)";
+
+    for (let i = 0; i < columns.length; i++) {
+        columns[i].addEventListener("click", function () {
+            if (columns[i].getAttribute("value") === "1") {
+                columns[i].setAttribute("value", "2");
+                columns[i].style.background = "radial-gradient(rgba(0, 255, 0, .45), rgba(0, 255, 0, .55), rgba(0, 255, 0, .75) 85%)";
                 score -= 2;
-            } else if (column.getAttribute("value") === "2") {
-                column.setAttribute("value", "3");
-                column.style.background = "rgba(0, 255, 0, 0)";
+            } else if (columns[i].getAttribute("value") === "2") {
+                columns[i].setAttribute("value", "3");
+                columns[i].style.background = "rgba(0, 255, 0, 0)";
                 score -= 3;
-            } else if (column.getAttribute("value") === "3") {
+            } else if (columns[i].getAttribute("value") === "3") {
                 return;
             }
+
+            document.getElementById("time-amount").textContent = timeAmount;
+            document.getElementById("score").textContent = score;
+
+            startTimer();
         });
-    });
+    }
 }
 
-// reset timer, score, and blocks to green
+// timer stops and resets when clicking on difficulty btns
+function stopTimer () {
+    clearInterval(timer);
+}
+
+// timer starts when score hits 98 (first block click)
+function startTimer () {
+    if (score === (s-2)) {
+        timer = setInterval(function () {
+            timeAmount--;
+            score--;
+
+            document.getElementById("time-amount").textContent = timeAmount;
+            document.getElementById("score").textContent = score;
+            
+            if (timeAmount <= 0) {
+                columns.forEach(function (column) {
+                    column.disabled = true;
+                });
+            }
+        }, 1000);
+    }
+}
+
+// reset timeAmount, score, blocks
 function resetIt () {
     while (gridBox.hasChildNodes()) {
         gridBox.removeChild(gridBox.firstChild);
     }
 
-    timer = 5;
-    score = 100;
-    document.getElementById("timer").textContent = timer;
-    document.getElementById("score").textContent = score;
+    timeAmount = g;
+    score = s;
+
+    stopTimer(timer);
 }
 
+// difficulty options
+normal.addEventListener("click", function () {
+    resetIt();
+    gridSize = g;
+    createGrid(gridSize);
+});
+hard.addEventListener("click", function () {
+    resetIt();
+    gridSize = 25;
+    createGrid(gridSize);
+})
+
+// runs when page loads
 createGrid(gridSize);
