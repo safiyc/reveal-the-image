2f9169dc872e290461aa4df5ffa520458e974a66
Safiy Cham <csafiy@yahoo.com>
2018-09-03 21:06:18 -0700

    Commit Error Fix: local repo is corrupted, error when attempting to push latest commits to github, now wholesale copying latest files from corrupted local repo and new commit here, deleting all broken commits

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
-A project using HTML/CSS/JS to let player guess what an image is by removing opaque blocks from a grid overlaying the image. Each time a block is removed, points are taken off. The goal is to pick the correct answer with as few moves as possible.
+# Reveal The Image
+### By Safiy Cham
 
-Author: Safiy Cham
+## Technologies used: JavaScript, HTML, CSS
+
+## Description
+
+A game involving guessing what an animal is by removing opaque blocks overlaying an animal image. Each time a block is removed, points are taken off. The goal is to pick the correct animal from a selection of animals with as few moves as possible to earn a high score.
+
+## Technical Details
+
+* Vanilla Javascript and plain HTML and CSS are used to create this program. No frameworks or libraries
+* LocalStorage is used to store high scores 
+* Scores are displayed from highest to lowest
+* Player can save initials along with score
+* Validation check is used to confirm initials are 2-3 characters long and they are alphabets
+* On page load and when game resets, a randomized animal from a selection of animals is chosen as the hidden image
+* Timer countdown starts at first clicked block
+* More info can be found in the "Rules" page of the game webpage (link below)
+
+## Pending Tasks
+* A larger pool of possible correct animals
+* Responsive Web Design
+* Revise to ES6
+* DRY
+
+## Links
+
+#### Launch Program - [Reveal The Image](https://safiyc.github.io/reveal-the-image/)
+
+#### [Github Repo](https://github.com/safiyc/Reveal-The-Image.git)
+
+#### [Portfolio](http://www.safiycham.com/)
+
+## Contact Info
+
+#### csafiy@yahoo.com
+
+## License
+
+This software is licensed under the MIT License
+
+Copyright 2018
--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const normal = document.querySelector("#normal");
 const hard = document.querySelector("#hard");
 
 let g = 16   // blocks in square grid
-let t = 10;   // time in seconds
+let t = 30;   // time in seconds
 let s = 500;   // score
 let gridSize = g;
 let timeAmount = t;
@@ -14,6 +14,8 @@ let score = s;
 let timer;
 let columns;
 let randomImgNum;
+
+let initialsScoreArr = [];
 
 // create grid; default 16x16 blocks
 function createGrid (gridSize) {
@@ -38,62 +40,212 @@ function createGrid (gridSize) {
     document.getElementById("time-amount").textContent = timeAmount;
     document.getElementById("score").textContent = score;
 
+    retrieveLocalStorage();  
     randomizeImg();
-    linkImgToAnswer();
     breakBlock();
 }
 
 // randomize gridBox bg img and return value of img
 function randomizeImg () {
-    randomImgNum = Math.floor(Math.random() * 3) + 1;
+    randomImgNum = Math.floor(Math.random() * 6) + 1;
 
     switch(randomImgNum) {
         case 1:
-            alert("1 - ball");
-            gridBox.style.backgroundImage = "url('./img/soccerball.jpg')";
+            gridBox.style.height = "400px";
+            gridBox.style.width = "400px";
+            gridBox.style.backgroundImage = "url('./img/beaver.jpg')";
             break;
         case 2:
-            alert("2 - rock");
-            gridBox.style.backgroundImage = "url('./img/rock.png')";
+            gridBox.style.height = "400px";
+            gridBox.style.width = "400px";
+            gridBox.style.backgroundImage = "url('./img/cats.jpg')";
             break;
         case 3:
-            alert("3 - disc");
-            gridBox.style.backgroundImage = "url('./img/frisbees.jpg')";
+            gridBox.style.height = "400px";
+            gridBox.style.width = "400px";
+            gridBox.style.backgroundImage = "url('./img/dog.jpg')";
+            break;
+        case 4:
+            gridBox.style.height = "400px";
+            gridBox.style.width = "400px";
+            gridBox.style.backgroundImage = "url('./img/fox.jpg')";
+            break;
+        case 5:
+            gridBox.style.height = "400px";
+            gridBox.style.width = "400px";
+            gridBox.style.backgroundImage = "url('./img/rabbit.jpg')";
+            break;
+        case 6:
+            gridBox.style.height = "400px";
+            gridBox.style.width = "400px";
+            gridBox.style.backgroundImage = "url('./img/raccoon.jpg')";
     }
 }
 
-// compare answer to img and display outcome
+// compare img and answer, display correct/wrong
+// if wrong, points off until correct answer
+// if correct, disable answer selections, stop timer; run functions
 function linkImgToAnswer () {
     let answerBtns = document.querySelectorAll(".answer");
+    let resultDisplay = document.querySelector(".result-display");
+    let playAgain = document.querySelector("#mssg-playagain");
 
     for (let i = 0; i < answerBtns.length; i++) {
         answerBtns[i].addEventListener("click", function () {
+            // if answer correct
             if (answerBtns[i].getAttribute("value") == randomImgNum) {
-                console.log("correct match: ", randomImgNum);
-                // need to display win message
-                // need to reveal img
+                answerBtns[i].setAttribute("style", "background-color: lime; border: 1px solid black;");
+                resultDisplay.setAttribute("style", "background-color: lime;");
+                resultDisplay.textContent = "Correct!";
+                playAgain.setAttribute("style", "visibility: visible;");
+                
+                while (gridBox.hasChildNodes()) {
+                    gridBox.removeChild(gridBox.firstChild);
+                }
+                
+                // stops time countdown and disables answers
+                stopTimer();
+                answerBtns.forEach(function (answer) {
+                    answer.disabled = true;
+                    answer.setAttribute("style", "background-color: dimgrey; border: none;")
+                });
+                
+                // retrieve from localStorage
+                retrieveLocalStorage();
+                // create initials-score object; display initials-score object
+                // push to object to array; sort from high to low
+                scoresToArrSort();
+                // save to localStorage
+                saveToLocalStorage();
+                // display top 5 scores (loop thru sorted array of objects)
+                displayHighToLow();
 
-                // implement localstorage to save score?
+            // if answer wrong
             } else {
-                score -= 50;
-                console.log("incorrect match");
-                // when to display lose message? When score to 0? After all answers are clicked?
+                score -= 100;
+                answerBtns[i].setAttribute("style", "background-color: red; border: 1px solid black;");
+                resultDisplay.setAttribute("style", "background-color: red;");
+                resultDisplay.textContent = "Wrong!";
+                
+                setTimeout(function () {
+                    resultDisplay.innerHTML = "Guess It!";
+                    answerBtns[i].removeAttribute("style", "background-color");
+                    resultDisplay.removeAttribute("style", "background-color");
+                }, 500);
             }
+
             document.getElementById("score").textContent = score;
         });
+    }  
+}
+
+// create object to store initials/score, and initials validation
+function saveScoreObj () {
+    const yourScore = document.querySelector(".your-score");
+    let userInput = prompt("You answered correct. Enter your initials of 2 to 3 letters.").toUpperCase();
+    let initials;
+
+    // validate initials
+    while (userInput !== /[A-Z]+/g) {
+        if (/[^A-Z]+/g.test(userInput)) {
+            userInput = prompt("Please enter only letters.").toUpperCase();
+        } else if (userInput.length < 2 || userInput.length > 3) {
+            userInput = prompt("Please enter 2 to 3 letters for your initials.").toUpperCase();
+        } else {
+            initials = userInput;
+            break;
+        }
+    }
+    
+    // create object for initials and score
+    let entry = {
+        initials: initials,
+        score: score
+    }
+
+    // score less than -500 shows up as -500
+    if (score <= -500) {
+        entry.score = -500;
+    }
+
+    yourScore.textContent = entry.initials + " " + entry.score;
+
+    return entry;
+}
+
+// push objects into array and sort from high to low
+function scoresToArrSort () {
+    initialsScoreArr.push(saveScoreObj());
+    
+    initialsScoreArr.sort(function (a, b) {
+        return b.score - a.score;
+    });
+}
+
+// display from high to low; limit 5 items listed
+function displayHighToLow () {
+    let ul = document.querySelector(".ranking-list");
+
+    while (ul.hasChildNodes()) {
+        ul.removeChild(ul.firstChild);
+    }
+
+    for (let i = 0; i < initialsScoreArr.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = initialsScoreArr[i].initials + " " + initialsScoreArr[i].score;
+        if (i < 5) {
+            ul.appendChild(li);
+        }
     }
 }
 
-// click block to reduce opacity; score is reduced
+// save to localStorage
+function saveToLocalStorage () {
+    localStorage.setItem("items", JSON.stringify(initialsScoreArr));
+}
+
+// retrieve from localStorage
+function retrieveLocalStorage () {
+    let localStorageData = JSON.parse(localStorage.getItem("items"));
+
+    if (localStorage.getItem("items")) {
+        initialsScoreArr = JSON.parse(localStorage.getItem("items"));
+    } else {
+        initialsScoreArr = [];
+        localStorageData = [];
+    }
+
+    localStorageData.forEach(function (key) {
+        displayHighToLow();
+    });
+}
+
+// remove data from localStorage
+const deleteLocalStorage = document.querySelector("#delete-scores");
+deleteLocalStorage.addEventListener("click", function () {
+    let ul = document.querySelector(".ranking-list");
+    
+    localStorage.clear();
+    
+    while (ul.hasChildNodes()) {
+        ul.removeChild(ul.firstChild);
+    }
+    
+    document.querySelector(".your-score").textContent = "";
+});
+
+// click block to reduce opacity; score is reduced; timer starts at first click
 function breakBlock () {
+    let arr = [];  // hacky way to invoke startTimer() only on 1st block click
     columns = document.querySelectorAll(".column-btns");
 
     for (let i = 0; i < columns.length; i++) {
         columns[i].addEventListener("click", function () {
             if (columns[i].getAttribute("value") === "1") {
                 columns[i].setAttribute("value", "2");
-                columns[i].style.background = "radial-gradient(rgba(0, 255, 0, .45), rgba(0, 255, 0, .55), rgba(0, 255, 0, .75) 85%)";
+                columns[i].style.background = "radial-gradient(rgba(0, 255, 0, .45), rgba(0, 255, 0, .55), rgba(0, 255, 0, .80) 70%)";
                 score -= 2;
+                arr.push(i);
             } else if (columns[i].getAttribute("value") === "2") {
                 columns[i].setAttribute("value", "3");
                 columns[i].style.background = "rgba(0, 255, 0, 0)";
@@ -101,63 +253,95 @@ function breakBlock () {
             } else if (columns[i].getAttribute("value") === "3") {
                 return;
             }
-
+            
             document.getElementById("time-amount").textContent = timeAmount;
             document.getElementById("score").textContent = score;
 
-            startTimer();
+            // invoke startTimer() only once at first block click
+            if (arr.length === 1) {
+                startTimer();
+            }
         });
     }
 }
 
-// timer stops and resets when clicking on difficulty btns
+// time countdown starts at first block click
+function startTimer () {
+    timer = setInterval(function () {
+        timeAmount--;
+        score--;
+        
+        document.getElementById("time-amount").textContent = timeAmount;
+        document.getElementById("score").textContent = score;
+        
+        if (timeAmount <= 0) {
+            score--;
+            columns.forEach(function (column) {
+                column.disabled = true;
+            });
+        }
+    }, 1000);
+}
+
+// time countdown stops
 function stopTimer () {
     clearInterval(timer);
 }
 
-// timer starts when score hits 98 (first block click)
-function startTimer () {
-    if (score === (s-2)) {
-        timer = setInterval(function () {
-            timeAmount--;
-            score--;
-
-            document.getElementById("time-amount").textContent = timeAmount;
-            document.getElementById("score").textContent = score;
-            
-            if (timeAmount <= 0) {
-                columns.forEach(function (column) {
-                    column.disabled = true;
-                    // display blocks disabled; time to guess?
-                });
-            }
-        }, 1000);
-    }
-}
-
-// reset timeAmount, score, blocks
+// reset blocks and answers
 function resetIt () {
+    let answerBtns = document.querySelectorAll(".answer");
+    let resultDisplay = document.querySelector(".result-display");
+    let playAgain = document.querySelector("#mssg-playagain");
+    document.querySelector(".your-score").textContent = "";
+
     while (gridBox.hasChildNodes()) {
         gridBox.removeChild(gridBox.firstChild);
     }
 
-    timeAmount = t;
-    score = s;
+    answerBtns.forEach(function (answer) {
+        answer.removeAttribute("style");
+        answer.disabled = false;
+    });
+    resultDisplay.removeAttribute("style");
+    resultDisplay.innerHTML = "Guess It!";
+    playAgain.setAttribute("style", "visibility: hidden;");
 
-    stopTimer(timer);
+    stopTimer(); 
 }
 
-// difficulty options
+// difficulty option choices
 normal.addEventListener("click", function () {
     resetIt();
     gridSize = g;
+    timeAmount = t;
+    score = s;
     createGrid(gridSize);
 });
 hard.addEventListener("click", function () {
     resetIt();
-    gridSize = 25;
+    gridSize = g + 4;
+    timeAmount = t;
+    score = s + 50;
     createGrid(gridSize);
-})
+});
 
-// runs when page loads
+// toggle display rules
+let rulesBtn = document.querySelector("#rules-btn");
+rulesBtn.addEventListener("click", function () {
+    let rulesBox = document.querySelector(".rules-box");
+    if (rulesBox.classList.contains("hidden")) {
+        rulesBox.classList.remove("hidden");
+    } else {
+        rulesBox.classList.add("hidden");
+    }
+});
+
+// get current year
+let date = new Date();
+document.getElementById("copyright").textContent = date.getFullYear();
+
+// run when page loads
 createGrid(gridSize);
+linkImgToAnswer();
+retrieveLocalStorage();
